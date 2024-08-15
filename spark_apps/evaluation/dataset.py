2d9a4dee158aa891c8876e9a1891058fae221985
Ajay Penmatcha <ajay.penmatcha@bluerivertech.com>
2024-08-15 03:08:08 -0700

    Working npz conversion. Stil have memory issues

--- spark_apps/evaluation/dataset.py
+++ spark_apps/evaluation/dataset.py
@@ -20,6 +20,12 @@ import json
 from brtdl.data.labelmap import add_ground_label, generate_correspondence_map, validate_remap_compatibility
 from brtdl.data._sync_labelmap import labelmap_remap
 from brtdl.data import SampleKeys
+import imgaug.augmenters as iaa
+from brtdl.transforms import custom_transforms
+from brtdl.data.labelmap import retrieve_labelmap
+from pyspark.sql.functions import col
+from brtdl.transforms import SegmentationTransform
+
 
 # Custom pyspark type containing numpy bytes and the dtype + dimensions of the image
 PySparkNumpyType = StructType([
@@ -29,29 +35,27 @@ PySparkNumpyType = StructType([
 ])
 
 AnnotationType = StructType([
-    StructField("annotation_data", StringType(), nullable=False),
+    StructField("annotation_data", PySparkNumpyType, nullable=False),
     StructField("labelmap", StringType(), nullable=False)
 ])
 
 # Convert a numpy array to PySparkNumpyType
 def numpy_to_pyspark(ndarray):
-    return PySparkNumpyType(
-        image_data=base64.b64encode(ndarray.tobytes()).decode('utf-8'),
-        dtype=ndarray.dtype.name,
-        dimensions=tuple(ndarray.shape)
-    )
+    return (base64.b64encode(ndarray.tobytes()).decode('utf-8'),
+        ndarray.dtype.name,
+        tuple(ndarray.shape),)
 
 def pyspark_to_numpy(pyspark_ndarray):
     return np.frombuffer(base64.b64decode(pyspark_ndarray.image_data), pyspark_ndarray.dtype).reshape(pyspark_ndarray.dimensions)
 
 
 # Input S3 location => Output base64 encoded bytes of the image
-class S3ImageDownloader(Transformer, Identifiable, HasOutputCol):
+class S3ImageDownloader(Transformer, HasOutputCol):
     def __init__(self, s3_key_col, s3_bucket_col, output_col):
         super().__init__()
         self.s3_key_col = s3_key_col
         self.s3_bucket_col = s3_bucket_col
-        self.setParams(outputCol=output_col)
+        self.outputCol = output_col
 
     def _transform(self, dataset):
         # Define UDF to process each row
@@ -63,23 +67,20 @@ class S3ImageDownloader(Transformer, Identifiable, HasOutputCol):
             img = np.array(Image.open(io.BytesIO(img_data)))
             return numpy_to_pyspark(img)
 
-        im_download_udf = udf(lambda s3_key, s3_bucket: im_download(s3_key, s3_bucket), PySparkNumpyType())
-        return dataset.withColumn(self.getOutputCol(), im_download_udf(dataset[self.s3_key_col], dataset[self.s3_bucket_col]))
+        im_download_udf = udf(lambda s3_key, s3_bucket: im_download(s3_key, s3_bucket), PySparkNumpyType)
+        return dataset.withColumn(self.outputCol, im_download_udf(dataset[self.s3_key_col], dataset[self.s3_bucket_col]))
 
 
 class LabelmapSync(Transformer, Identifiable):
     def __init__(self, to_labelmap, correspondence_map, fix_ground):
         super().__init__()
-        self.to_labelmap = to_labelmap
-        self.correspondence_map = correspondence_map
+        self.to_labelmap = retrieve_labelmap(to_labelmap)
+        self.correspondence_map = retrieve_labelmap(correspondence_map)
         self.fix_ground = fix_ground
 
     def _transform(self, dataset):
         def sync_labelmap(annotation_data, labelmap):
-            try:
-                from_labelmap = {int(key): val for key, val in json.loads(labelmap).items()}
-            except json.decoder.JSONDecodeError as e:
-                raise ValueError(f"Invalid labelmap: {labelmap}") from e
+            from_labelmap = {int(key): val for key, val in labelmap.items()}
 
             if self.fix_ground:
                 add_ground_label(from_labelmap)
@@ -105,14 +106,36 @@ class LabelmapSync(Transformer, Identifiable):
             synced_annotation = labelmap_remap(annotation, c_idx_map)
             return (numpy_to_pyspark(synced_annotation), self.to_labelmap)
     
-        sync_labelmap_udf = udf(lambda annotation_data, labelmap: sync_labelmap(annotation_data, labelmap), AnnotationType())
-        return dataset.withColumn("synced_annotation", sync_labelmap_udf(dataset["annotation"], dataset["labelmap"])).drop("annotation_data")
+        sync_labelmap_udf = udf(lambda annotation_data, labelmap: sync_labelmap(annotation_data, labelmap), AnnotationType)
+        return dataset.withColumn(
+            "synced_annotation_data",
+            sync_labelmap_udf(dataset["annotation_data"], dataset["annotation_pixelwise_0_label_map"])
+        ).drop("annotation_data").select(
+            "*",
+            col("synced_annotation_data.annotation_data").alias("annotation_data"), 
+            col("synced_annotation_data.labelmap").alias("annotation_pixelwise_0_label_map")
+        ).drop("synced_annotation_data")
 
+_image_data_type_transform_map = {
+    'uint8': custom_transforms.ToUint8,
+    'float16': custom_transforms.ToFloat16,
+    'float32': custom_transforms.ToFloat32
+}
 
-class NpzPackager(Transformer, Identifiable, HasOutputCol):
-    def __init__(self, outputCol):
+class NpzPackager(Transformer, HasOutputCol):
+    def __init__(self, input_image_modes, output_image_modes, input_shape, image_data_type):
         super().__init__()
-        self.setParams(outputCol=outputCol)
+        self.outputCol = "npz_data"
+        self.input_image_modes = input_image_modes
+        self.output_image_modes = output_image_modes
+        self.input_shape = input_shape
+        self.image_data_type = image_data_type
+        self.my_transform = SegmentationTransform(iaa.Sequential([
+            custom_transforms.ImageFormat(input_mode=self.input_image_modes, output_mode=self.output_image_modes),
+            custom_transforms.ToFloat32(),
+            custom_transforms.ResizeImageLabel(image_size=self.input_shape, label_size=self.input_shape),
+            _image_data_type_transform_map[self.image_data_type](),
+        ]))
 
     def _transform(self, dataset):
         def pack_npz(image_data, annotation_data):
@@ -122,14 +145,15 @@ class NpzPackager(Transformer, Identifiable, HasOutputCol):
                 SampleKeys.image: image,
                 SampleKeys.label: annotation
             }
+            sample = self.my_transform(sample)
             bytes_io = io.BytesIO()
             np.savez_compressed(bytes_io, **sample)
             return base64.b64encode(bytes_io.getvalue()).decode('utf-8')
         pack_npz_udf = udf(lambda annotation_data, annotation_labelmap: pack_npz(annotation_data, annotation_labelmap), StringType())
-        return dataset.withColumn(self.getOutputCol(), pack_npz_udf(dataset["annotation"], dataset["labelmap"]))
+        return dataset.withColumn(self.outputCol, pack_npz_udf(dataset["image_data"], dataset["annotation_data"]))
 
 
-class ImageDimensionExtractor(Transformer, Identifiable, HasInputCol, HasOutputCol):
+class ImageDimensionExtractor(Transformer, HasInputCol, HasOutputCol):
     def __init__(self, inputCol, outputCol):
         super(ImageDimensionExtractor, self).__init__()
         self._setDefault(inputCol=inputCol, outputCol=outputCol)
@@ -150,29 +174,29 @@ class ImageDimensionExtractor(Transformer, Identifiable, HasInputCol, HasOutputC
 
 
 ds = Dataset.retrieve('66b0f40dab0fb042364a859e')
-dset_df = ds.to_dataframe().astype(str)
+dset_df = ds.to_dataframe()[['artifact_nrg_0_web_s3_key', 'artifact_nrg_0_web_s3_bucket', 'annotation_pixelwise_0_s3_key', 'annotation_pixelwise_0_s3_bucket', 'annotation_pixelwise_0_label_map']]
 print(len(dset_df))
-dset_df = dset_df.head(20)
+dset_df = dset_df.head(200)
 
 spark = SparkSession.builder.appName(
     "Evaluation: Datasets"
 ).getOrCreate()
 
-spark_df = spark.createDataFrame(dset_df)
+spark_df = spark.createDataFrame(dset_df).persist(storageLevel="DISK_ONLY")
 
-image_download = S3ImageDownloader(s3_key_col='artifact_nrg_0_web_s3_key', s3_bucket_col='artifact_nrg_0_web_s3_bucket', output_col='nrg', dtype=np.float32)
-spark_df = image_download.transform(spark_df)
+image_download = S3ImageDownloader(s3_key_col='artifact_nrg_0_web_s3_key', s3_bucket_col='artifact_nrg_0_web_s3_bucket', output_col='image_data')
+spark_df = image_download.transform(spark_df).persist(storageLevel="DISK_ONLY")
 
-anno_download = S3ImageDownloader(s3_key_col='annotation_pixelwise_0_s3_key', s3_bucket_col='annotation_pixelwise_0_s3_bucket', output_col='annotation', dtype=np.int8)
-spark_df = anno_download.transform(spark_df)
+anno_download = S3ImageDownloader(s3_key_col='annotation_pixelwise_0_s3_key', s3_bucket_col='annotation_pixelwise_0_s3_bucket', output_col='annotation_data')
+spark_df = anno_download.transform(spark_df).persist(storageLevel="DISK_ONLY")
 
-labelmap_sync = LabelmapSync(to_labelmap=ds.labelmap, correspondence_map=ds.correspondence_map, fix_ground=True)
-spark_df = labelmap_sync.transform(spark_df)
+labelmap_sync = LabelmapSync(to_labelmap="DEFAULT_LABELMAP", correspondence_map="DEFAULT_C_MAP", fix_ground=True)
+spark_df = labelmap_sync.transform(spark_df).persist(storageLevel="DISK_ONLY")
 
-npz_packager = NpzPackager(outputCol='nrg_annotation')
-spark_df = npz_packager.transform(spark_df)
+npz_packager = NpzPackager(input_image_modes='nrg', output_image_modes='nrg', input_shape=(540, 960), image_data_type='float32')
+spark_df = npz_packager.transform(spark_df).persist(storageLevel="DISK_ONLY")
 
-dimension_extractor = ImageDimensionExtractor(inputCol='nrg', outputCol='nrg_dimension')
-spark_df = dimension_extractor.transform(spark_df)
+# dimension_extractor = ImageDimensionExtractor(inputCol='nrg', outputCol='nrg_dimension')
+# spark_df = dimension_extractor.transform(spark_df)
 
-spark_df.select('nrg_dimension').show()
+spark_df.select('npz_data').show()
